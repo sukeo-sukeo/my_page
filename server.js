a52e44f2d43f5174f312db9de104e5811a03b323
sukeo-sukeo <syuke4522@gmail.com>
2021-01-12 05:04:05 +0900

    server.js

--- server.js
+++ server.js
@@ -17,24 +17,7 @@ app.use(bodyparser.json());
 
 app.use(express.static(__dirname + "/docs/"));
 
-//ページリロード時の対策...(未確定)
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/docs/index.html');
-});
-app.get('/product', (req, res) => {
-  res.sendFile(__dirname + '/docs/index.html');
-});
-app.get('/skill', (req, res) => {
-  res.sendFile(__dirname + '/docs/index.html');
-});
-app.get('/learned', (req, res) => {
-  res.sendFile(__dirname + '/docs/index.html');
-});
-app.get('/contact', (req, res) => {
-  res.sendFile(__dirname + '/docs/index.html');
-});
-
-//各ページのデータ取得の記述
+//api/以下をparamsで変数にすれば簡潔
 app.get('/api/home', (req, res) => {
   db.query("select * from home", (err, docs) => {
     if (err) console.log(err);
@@ -62,6 +45,55 @@ app.get('/api/learned', (req, res) => {
     res.json(docs)
   });
 });
+
+
+
+
+//ページリロード時の対策...(未確定)
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/docs/index.html');
+// });
+// app.get('/product', (req, res) => {
+//   res.sendFile(__dirname + '/docs/index.html');
+// });
+// app.get('/skill', (req, res) => {
+//   res.sendFile(__dirname + '/docs/index.html');
+// });
+// app.get('/learned', (req, res) => {
+//   res.sendFile(__dirname + '/docs/index.html');
+// });
+// app.get('/contact', (req, res) => {
+//   res.sendFile(__dirname + '/docs/index.html');
+// });
+
+// //各ページのデータ取得の記述
+// app.get('/api/home', (req, res) => {
+//   db.query("select * from home", (err, docs) => {
+//     if (err) console.log(err);
+//     res.json(docs)
+//   });
+// });
+
+// app.get('/api/product', (req, res) => {
+//   db.query("select * from product", (err, docs) => {
+//     if (err) console.log(err);
+//     res.json(docs);
+//   });
+// });
+
+// app.get('/api/skills', (req, res) => {
+//   db.query("select * from skills inner join skill_category on skills.category_id = skill_category.id", (err, docs) => {
+//     if (err) console.log(err);
+//     res.json(docs);
+//   });
+// });
+
+// app.get('/api/learned', (req, res) => {
+//   db.query("select * from learned", (err, docs) => {
+//     if (err) console.log(err);
+//     res.json(docs)
+//   });
+// });
 
 app.listen(port);
 
